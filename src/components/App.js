02cf9c962731e982f9d8070211cedff2bbdff06d
aditi944 <aditijha201200@gmail.com>
2022-06-10 17:51:14 +0530

    profile

--- src/components/App.js
+++ src/components/App.js
@@ -5,30 +5,30 @@ import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom"
 import Dashboard from "./Dashboard"
 import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
+// import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgetPassword"
 import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
+    // <Container
+    //   className="d-flex align-items-center justify-content-center"
+    //   style={{ minHeight: "100vh" }}
+    // >
+    //   <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
           <AuthProvider>
             <Routes>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
+              <Route exact path="/" element={<Dashboard/>} />
+              <Route path="/update-profile" element={<UpdateProfile/>} />
+              <Route path="/signup" element={<Signup/>} />
+              <Route path="/login" element={<Login/>} />
+              <Route path="/forgot-password" element={<ForgotPassword/>} />
             </Routes>
           </AuthProvider>
         </Router>
-      </div>
-    </Container>
+      /* </div>
+    </Container> */
   )
 }
 
